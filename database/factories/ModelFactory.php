bae967a0908204f09adca6582e8c5babdbd22ca2
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2017-02-05 16:31:42 -0500

    Fix TRUCO para que no se cree dos usuarios, usar FUNCIONES ANÓNIMAS

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -23,11 +23,14 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 });
 
 
-$factory->define(App\Post::class, function (Faker\Generator $faker) {
+$factory->define(App\Post::class, function (Faker\Generator $faker, $attributes) {
 
     return [
         'title'   => $faker->sentence,
         'content' => $faker->paragraph,
         'pending' => $faker->boolean,
+        'user_id' => function () {
+            return factory(\App\User::class)->create()->id;
+        },
     ];
 });
